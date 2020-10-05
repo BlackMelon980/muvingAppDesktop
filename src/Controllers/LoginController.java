ffb72cf2c59f4061b3f077569e7ab2516f09eb75
FrancescaCapodanno <capofrancy@gmail.com>
2020-10-05 11:30:25 +0200

    aggiunto alert dialog con modifica estetica

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,14 +1,18 @@
 package Controllers;
 
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,6 +23,8 @@ public class LoginController extends Component {
     @FXML public Button loginButton;
     public TextField idAdmin;
     public TextField passwordAdmin;
+    public AnchorPane anchorPaneLogin;
+    public BorderPane borderPaneRoot;
     private Stage mainStage;
 
     LoginDialogController loginDialogController;
@@ -48,12 +54,14 @@ public class LoginController extends Component {
             if (result == 1){
                 isLogged = true;
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/reviewsPage.fxml"));
-
                 try {
                     Parent rootLayout = fxmlLoader.load();
                     Scene scene = new Scene(rootLayout);
                     scene.getStylesheets().add("/Layout/layout.css");
+                    Scene rootScene = anchorPaneLogin.getScene();
+                    rootLayout.translateYProperty().set(rootScene.getHeight());
                     mainStage.setScene(scene);
+
                     mainStage.setTitle("Applicazione");
 
                 }catch (IOException e){
